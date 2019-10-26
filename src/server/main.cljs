290c30113a72d34032c2c89923cc1ae549cba165
jiyinyiyong <jiyinyiyong@gmail.com>
2019-10-27 01:44:22 +0800

    locate the strange macro problem

--- src/server/main.cljs
+++ src/server/main.cljs
@@ -1,5 +1,6 @@
 
-(ns server.main)
+(ns server.main
+ (:require-macros [server.macro :refer [defcomp]]))
 
 (def value-a 1)
 
@@ -9,5 +10,8 @@
   (println "Code updated.")
   (println "Trying values:" value-a value-b))
 
+(defcomp comp-a [a] {:data a})
+
 (defn main! []
-  (println "App loaded!"))
+  (println "App loaded!")
+  (println (comp-a 100)))
